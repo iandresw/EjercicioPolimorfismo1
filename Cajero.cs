e000f023a3aade23f3f6ffdcdcb259be162e8724
iandresw <rbejarano@unah.hn>
2024-05-18 21:03:34 -0600

    Create Cajero.cs
    se creo la clase cajero que hereda de clase abstracta persona, para gestionar el acceso a caja

---
+++ Cajero.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPolimorfismo
+    
+{
+    public class Cajero : Persona
+
+    {
+       
+        public override string DNI()
+        {
+            return identidad;
+        }
+
+        public override string Nombre()
+        {
+            return PNombre + " " + PApellido;
+        }
+
+        public bool AccesoCaja()
+        {
+            return true;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("## DATOS EMPLEADO CAJERO ##");
+            Console.WriteLine("\n");
+            Console.WriteLine($"Nombre empleado: {Nombre()}");
+            Console.WriteLine($"DNI: {DNI()}");
+            Console.WriteLine($"Acceso a Caja: {AccesoCaja()}");
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("## ULTIMA LINEA ##");
+            Console.WriteLine("\n");
+        }
+    }
+}
