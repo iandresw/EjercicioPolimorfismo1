23161615fd1a3333bacae6e2bc6bebe92117e72d
iandresw <rbejarano@unah.hn>
2024-05-18 21:02:52 -0600

    Create Persona.cs
    Se creo la clase  abstracta persona con tres atributos encapsulados  y dos métodos abstractos

---
+++ Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPolimorfismo
+
+
+{
+  
+    public abstract class Persona
+    {
+
+        public string PNombre { get; set; }
+        public string PApellido { get; set; }
+        public string identidad { get; set; }
+        public abstract string Nombre();
+        public abstract string DNI();
+    
+    }
+}
