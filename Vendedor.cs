dd81cf8ee7a575e9b096bfd1b26901f74ff4b5ea
iandresw <rbejarano@unah.hn>
2024-05-18 21:04:08 -0600

    Create Vendedor.cs
    se creo la clase vendedor que hereda de clase abstracta persona y gestiona el acceso al inventario

---
+++ Vendedor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPolimorfismo
+{
+    internal class Vendedor : Persona
+    {
+        public override string DNI()
+        {
+            return identidad;
+        }
+
+        public override string Nombre()
+        {
+            return PNombre + " " + PApellido;
+        }
+
+        public bool AccesoInventario()
+        {
+            return true;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("## DATOS EMPLEADO VENDEDOR ##");
+            Console.WriteLine("\n");
+            Console.WriteLine($"Nombre empleado: {Nombre()}");
+            Console.WriteLine($"DNI: {DNI()}");
+            Console.WriteLine($"Acceso a inventario: {AccesoInventario()}");
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("## ULTIMA LINEA ##");
+            Console.WriteLine("\n");
+        }
+    }
+}
