08e06bef523619bcf2469905a28514fc38681ed7
iandresw <rbejarano@unah.hn>
2024-05-18 21:04:59 -0600

    Update Program.cs
    se crearon los objetos vendedor y cajero y se  ingresaron e imprimieron los datos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EjercicioPolimorfismo;
+
+
+Console.WriteLine("########### INICIO ###########");
+
+Vendedor vendedor = new Vendedor();
+Console.WriteLine("Ingrese el Nombre del empleado vendedor: ");
+
+vendedor.PNombre = Console.ReadLine();
+Console.WriteLine("Ingrese el Apellido del empleado vendedor: ");
+
+vendedor.PApellido = Console.ReadLine();
+
+Console.WriteLine("Ingrese la identidad del empleado vendedor: ");
+
+vendedor.identidad = Console.ReadLine();
+vendedor.Imprimir();
+
+Cajero cajero = new Cajero();
+
+Console.WriteLine("Ingrese el Nombre del empleado cajero: ");
+
+cajero.PNombre = Console.ReadLine();
+Console.WriteLine("Ingrese el Apellido del empleado cajero: ");
+
+cajero.PApellido = Console.ReadLine();
+
+Console.WriteLine("Ingrese la identidad del empleado cajero: ");
+
+cajero.identidad = Console.ReadLine();
+
+
+cajero.Imprimir();
